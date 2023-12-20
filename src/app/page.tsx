a82ef955c8d1cceebb47819ff6abe20aff9b1d87
sameer <sameersultan1001@gmail.com>
2023-12-20 12:40:25 +0500

    add layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import Main from "@/components/Main";
 import Porfolio from "@/components/Porfolio";
 import Pricing from "@/components/Pricing";
 import Services from "@/components/Services";
@@ -7,11 +8,13 @@ import Services from "@/components/Services";
 export default function Home() {
   return (
   <>
-  <Hero />
+  {/* <h1 className="bg-red-500">ok</h1> */}
+  <Main />
+  {/* <Hero />
   <About />
   <Services />
   <Pricing />
-  <Porfolio />
+  <Porfolio /> */}
   </>
   )
 }
