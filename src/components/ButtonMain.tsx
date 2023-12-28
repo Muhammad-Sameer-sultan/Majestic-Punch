7731f8d48109ccab3c092d2d6bcb4cdefc0633c3
sameer <sameersultan1001@gmail.com>
2023-12-28 11:28:34 +0500

    title change

--- src/components/ButtonMain.tsx
+++ src/components/ButtonMain.tsx
@@ -9,6 +9,7 @@ const ButtonMain = ({ Btntext,path }: any) => {
 
   return (
     <button
+    // type='submit'
       onClick={()=>{router.push(`/${path}`)}}
       className="rounded-26xl capitalize font-semibold hover:border-lightseagreen-100 border hover:shadow-xl hover:text-lightseagreen-100 hover:bg-white bg-lightseagreen-100 py-4 px-[45px] text-base sm:text-lg text-white font-poppins"
     >
