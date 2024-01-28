d8c7284846b7b8f924354e130de146431c618f66
Sameer <sameersultan1001@gmail.com>
2024-01-28 06:40:20 +0500

    'fix'

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -17,7 +17,7 @@ const Pricing = () => {
           <ButtonMain Btntext="Read more" path='pricing' />
         </div>
         <div className="text-base  font-poppins max-slg:mt-10 max-slg:w-full w-1/3">
-        Embark on your creative journey with Majestic Punch! Enjoy a Spectacular 50% Discount on Your First Order. Immerse yourself in high-quality embroidery and digitizing services tailored to your unique vision. Don't miss this chance to elevate your projects with precision and artistry. Place your order now and experience the Majestic Punch difference!
+        Embark on your creative journey with Majestic Punch! Enjoy a Spectacular 50% Discount on Your First Order. Immerse yourself in high-quality embroidery and digitizing services tailored to your unique vision. Don&apos;t miss this chance to elevate your projects with precision and artistry. Place your order now and experience the Majestic Punch difference!
         </div>
       </div>
 
